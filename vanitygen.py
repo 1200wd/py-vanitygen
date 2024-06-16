89ad26578e355831134636090a11e5346c3e75fb
Cryp Toon <info@1200wd.com>
2024-06-16 10:11:09 +0200

    Use bitcoinlib library

--- vanitygen.py
+++ vanitygen.py
@@ -20,50 +20,61 @@
 #
 
 import os
-from bitcoin import *
+# from bitcoin import *
+from bitcoinlib.keys import HDKey
 import timeit
 import random
 import multiprocessing
 
 
-def address_search(pipeout, search_for='12o'):
+witness_type = 'segwit'
+
+def address_search(search_for='bc1q2345'):
     privkey = random.randrange(2**256)
     address = ''
     count = 0
     start = timeit.default_timer()
 
-    os.write(pipeout, bytes("Searching for %s (pid %s)" % (search_for, os.getpid()), 'utf-8'))
+    print("Searching for %s (pid %s)" % (search_for, os.getpid()))
 
     while not search_for in address:
         privkey += 1
-        pubkey_point = fast_multiply(G, privkey)
-        address = pubkey_to_address(pubkey_point)
+        # pubkey_point = fast_multiply(G, privkey)
+        # address = pubkey_to_address(pubkey_point)
+        k = HDKey(witness_type=witness_type)
+        address = k.address()
         count += 1
-        if not count % 1000:
-            os.write(pipeout, bytes("Searched %d in %d seconds (pid %d)" % (count, timeit.default_timer()-start, os.getpid()), 'utf-8'))
+        if not count % 10000:
+            print("Searched %d in %d seconds (pid %d)" % (count, timeit.default_timer()-start, os.getpid()))
 
-    os.write(pipeout, bytes("Found address %s" % address, 'utf-8'))
-    os.write(pipeout, bytes("Private key HEX %s" % encode_privkey(privkey, 'hex'), 'utf-8'))
+    print("Found address %s" % address)
+    print("Private key HEX %s" % k.private_hex)
+    return((address, k.private_hex))
 
 
 def main():
-    # processors = multiprocessing.cpu_count()
-    # processors = 2
-    # print("You have %d processors so starting %d threads" % (processors, processors))
-    # for i in range(processors):
+    # print(multiprocessing.cpu_count())
+    processors = 8
+    print("Starting %d processes" % processors)
+    ps = []
+    for i in range(processors):
+        print("Starting process %d" % i)
+        p = multiprocessing.Process(target=address_search)
+        p.start()
+        ps.append(p)
+        # pipein, pipeout = os.pipe()
+        # pid = os.fork()
+        # if pid == 0:
+        #     os.close(pipein)
+        #     address_search(pipeout)
+        # else:
+        #     pipein = os.fdopen(pipein)
+            # while True:
+            #     line = os.read(pipein, 32)
+            #     print(line)
 
-    pipein, pipeout = os.pipe()
-    pid = os.fork()
-    if pid == 0:
-        os.close(pipein)
-        address_search(pipeout)
-    else:
-        # pipein = os.fdopen(pipein)
-        while True:
-            line = os.read(pipein, 32)
-            print(line)
-
-    print('Main process exiting')
+    # print(ps)
+    # print('Main process exiting')
 
 
 main()
